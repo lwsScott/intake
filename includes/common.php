5166dfa6e07533c06ec714b88c0295b544d6080c
Lewis Scott <lscott19@mail.greenriver.edu>
2021-03-05 15:21:19 -0500

    edited includes files to make consistent with routing

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,8 @@
 <?php
-require("/home/stjamesk/dotcom/creds/creds.php");
+//require("/home/stjamesk/dotcom/creds/creds.php");
+require_once $_SERVER['DOCUMENT_ROOT']."/../config.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/../db.php";
+
 // This class is able to display all records and delete individual ones
 class Common
 {
@@ -15,7 +18,11 @@ class Common
         $imageQuery = "SELECT `Attachments` FROM `outreach_form` WHERE id='$recordId'";
         $imagePath = $conn->query($imageQuery) or die("Error in query2".$conn->error);
         $path = mysqli_fetch_array($imagePath);
-        unlink("/home/dotcomgr/public_html/$path[0]");
+        var_dump($path);
+        //unlink("/home/dotcomgr/public_html/$path[0]");
+        if ($path[0] != "uploads/") {
+            unlink("/home2/lscottgr/public_html/intake/$path[0]");
+        }
         $query = "DELETE FROM outreach_form WHERE id='$recordId'";
         $result = $conn->query($query) or die("Error in query3".$conn->error);
         return $result;
--- includes/delete.php
+++ includes/delete.php
@@ -1,5 +1,8 @@
 <?php
-require "/home/stjamesk/dotcom/creds/creds.php";
+//require "/home/stjamesk/dotcom/creds/creds.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/../config.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/../db.php";
+
 include_once "common.php";
 // if record is set in the $_GET array, then delete the row
 if (isset($_GET['recordId'])) {
@@ -7,9 +10,12 @@ if (isset($_GET['recordId'])) {
     // construct a new Common() class
     $common = new Common();
     // delete the row
-    $delete = $common->deleteRecordById($conn, $recordId);
+    //$delete = $common->deleteRecordById($conn, $recordId);
+    $delete = $common->deleteRecordById($cnxn, $recordId);
     // if delete works, redirect page back to control page
     if ($delete) {
-        echo '<script>window.location.href="../control.php";</script>';
+        //echo '<script>window.location.href="../control.php";</script>';
+        echo '<script>window.location.href="../control";</script>';
+
     }
 }
